54738c93a0b47d587e7428221259f4c282cdfa1f
Mark Blanco <markb1@andrew.cmu.edu>
2018-04-12 13:05:55 +0000

    Add scripts to update gem5-aladdin source from repository folder.

---
+++ mat_soln_with_cpu/update_from_repo.sh
@@ -0,0 +1,9 @@
+ACCEL_NAME=mat_soln
+GIT_DIR=/workspace/CompArchMIP/${ACCEL_NAME}_with_cpu/
+SRC_DIR=/workspace/gem5-aladdin/src/aladdin/integration-test/with-cpu/${ACCEL_NAME}/
+CMD=rsync
+$CMD ${GIT_DIR}*.c $SRC_DIR
+$CMD ${GIT_DIR}*.cfg $SRC_DIR
+$CMD ${GIT_DIR}*.h $SRC_DIR
+$CMD ${GIT_DIR}Makefile $SRC_DIR
+$CMD ${GIT_DIR}copy_to_repo.sh $SRC_DIR
