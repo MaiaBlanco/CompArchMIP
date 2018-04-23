9d5c5bb0b954d1e191ce97889eac5c3e3f3aa91a
Mark Blanco <markb1@andrew.cmu.edu>
2018-04-23 05:42:54 +0000

    Add python files to updater scripts.

--- mat_inv_with_cpu/update_from_repo.sh
+++ mat_inv_with_cpu/update_from_repo.sh
@@ -2,6 +2,7 @@ ACCEL_NAME=mat_inv
 GIT_DIR=/workspace/CompArchMIP/${ACCEL_NAME}_with_cpu/
 SRC_DIR=/workspace/gem5-aladdin/src/aladdin/integration-test/with-cpu/${ACCEL_NAME}/
 CMD=rsync
+$CMD ${GIT_DIR}*.py $SRC_DIR
 $CMD ${GIT_DIR}*.c $SRC_DIR
 $CMD ${GIT_DIR}*.cfg $SRC_DIR
 $CMD ${GIT_DIR}*.h $SRC_DIR
